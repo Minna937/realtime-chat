2d8842cb861596f50192911e916e7adea6855020
Minna <liu_minna@hotmail.com>
2023-05-31 11:07:30 +0200

    updates button variant

--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -1,11 +1,30 @@
-import { FC } from 'react'
+import { cva } from "class-variance-authority"
+import { FC } from "react"
 
-interface ButtonProps {
-  
-}
+interface ButtonProps {};
 
-const Button: FC<ButtonProps> = ({}) => {
-  return <div>Button</div>
+const buttonVariants = cva(
+    "active:scal-95 inline-flex items-center jusitify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+    {
+        variants: {
+            variant: {
+                default: "bg-slate-900 text-white hover:bg-slate-800",
+                ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200"
+            },
+            size: {
+                default: "h-10 py-2 px-4",
+                sm: "h-9 px-2",
+                lg: "h-11 px-8",
+            }
+        },
+        defaultVariants: {
+            variant: "default",
+            size: "default"
+        },
+    })
+
+const Button: FC<ButtonProps> = ({ }) => {
+    return <div>Button</div>
 }
 
 export default Button
