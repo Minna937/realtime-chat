74c6e22f374aec5d57c2bea413a5e6c18225dcb8
Minna <liu_minna@hotmail.com>
2023-05-31 19:01:31 +0200

    updates authentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,3 +1,4 @@
-import NextAuth from "next-auth/next"
+import NextAuth from "next-auth/next";
+import { authOptions } from "@/lib/auth";
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
