3b50dac7a241687ec716b0bec28ebb6e287f583f
Minna <liu_minna@hotmail.com>
2023-06-12 12:16:57 +0200

    updating api route for adding friends

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,18 @@
+import { addFriendValidator } from "@/lib/validations/add-friend";
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+
+        const { email: emailToAdd } = addFriendValidator.parse(body.email);
+
+        const RESTResponse = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/user:email:${emailToAdd}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`
+            },
+            cache: `no-store`,
+        })
+    } catch (error) {
+
+    }
+}
