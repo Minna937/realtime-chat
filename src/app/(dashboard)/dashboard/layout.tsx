d6e42988b9378ce8a29f8b370ecb2390cae1521a
Minna <liu_minna@hotmail.com>
2023-06-13 11:01:06 +0200

    updates layout for dashboard add

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -4,6 +4,7 @@ import { FC, ReactNode } from 'react';
 import { notFound } from "next/navigation";
 import Link from "next/link";
 import { Icon, Icons } from "@/components/Icons";
+import Image from "next/image";
 
 interface LayoutProps {
     children: ReactNode
@@ -96,8 +97,8 @@ const Layout = async ({ children }: LayoutProps) => {
                                            leading-6 
                                            font-semibold
                                            ">
-                                            <span 
-                                            className="
+                                            <span
+                                                className="
                                             text-gray-400
                                             border-gray-200 
                                             group-hover:border-indigo-600 
@@ -124,6 +125,43 @@ const Layout = async ({ children }: LayoutProps) => {
                                 )
                             })}
                         </ul>
+
+                        <li className="
+                        -mx-6 
+                        mt-auto
+                        flex
+                        items-center">
+                            <div className="
+                            flex 
+                            flex-1 
+                            items-center 
+                            gap-x-4 
+                            px-6 
+                            py-3 
+                            text-sm 
+                            font-smibold 
+                            leading-6
+                            text-gray-900">
+                                <div className="relative h-8 w-8 bg-gray-50">
+                                    <Image
+                                        fill
+                                        referrerPolicy="no-referrer"
+                                        className="rounded-full"
+                                        src={session.user.image || ""}
+                                        alt="Your profile picture"
+                                    />
+                                </div>
+                                <span className="sr-only">Your profile</span>
+                                <div className="flex flex-col">
+                                    <span aria-hidden="true">{session.user.name}</span>
+                                    <span
+                                        className="text-xs text-zinc-400"
+                                        aria-hidden="true">
+                                        {session.user.email}
+                                    </span>
+                                </div>
+                            </div>
+                        </li>
                     </ul>
                 </nav>
             </div>
