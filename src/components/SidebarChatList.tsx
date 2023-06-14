f23e5ce4f3860395f7bbba98c69e3c0c9e9ed674
Minna <liu_minna@hotmail.com>
2023-06-14 12:12:27 +0200

    updates sidebarchat component

--- src/components/SidebarChatList.tsx
+++ src/components/SidebarChatList.tsx
@@ -1,10 +1,26 @@
-import { FC } from 'react';
+"use client";
+
+import { usePathname, useRouter } from "next/navigation";
+import { FC, useEffect, useState } from 'react';
 
 interface SidearChatListProps {
   friends: User[]
 };
 
 const SidearChatList: FC<SidearChatListProps> = ({ friends }) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [unseenMessages, setUnseenMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    if (pathname?.includes('chat')) {
+      setUnseenMessages((prev) => {
+        return prev.filter((msg) => !pathname.includes(msg.senderId))
+      })
+    }
+  }, [pathname])
+
   return <ul role="list"
     className="
     max-h-[25rem]
@@ -12,9 +28,9 @@ const SidearChatList: FC<SidearChatListProps> = ({ friends }) => {
     -mx-2
     space-y-1
     ">
-    {/* {friends.sort().map((friend)=>{
-
-    })} */}
+    {friends.sort().map((friend) => {
+      return <div></div>
+    })}
   </ul>
 };
 
