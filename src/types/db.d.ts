e4ada7bca145d6bca5b631bb3c6201a88d149fb1
Minna <liu_minna@hotmail.com>
2023-06-14 10:58:51 +0200

    updates types and chat page

--- src/types/db.d.ts
+++ src/types/db.d.ts
@@ -3,4 +3,24 @@ interface User {
     email: string
     image: string
     id: string
+};
+
+interface Chat {
+    id: string
+    messages: Message[]
+};
+
+
+interface Message {
+    id: string
+    senderId: string
+    receivedId: string
+    text: string
+    timestamp: number
+};
+
+interface FriendRequest {
+    id: string
+    senderId: string
+    receivedId: string
 }
