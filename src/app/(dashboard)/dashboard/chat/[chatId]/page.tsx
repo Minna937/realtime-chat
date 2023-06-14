2c1d1b2a7f31c6802b4bb26fe3b01f615f7ebbeb
Minna <liu_minna@hotmail.com>
2023-06-14 14:06:22 +0200

    renders chat's friend's portfolio

--- src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
+++ src/app/(dashboard)/dashboard/chat/[chatId]/page.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { messageArrayValidator } from "@/lib/validations/message";
 import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 import { FC } from 'react';
 
 interface PageProps {
@@ -22,9 +23,7 @@ async function getChatMessages(chatId: string) {
     );
 
     const dbMessgaes = results.map((message) => JSON.parse(message) as Message);
-
     const reversedDbMessages = dbMessgaes.reverse();
-
     const messages = messageArrayValidator.parse(reversedDbMessages);
 
     return messages;
@@ -51,7 +50,55 @@ const page = async ({ params }: PageProps) => {
   const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
   const initialMessages = await getChatMessages(chatId);
 
-  return <div>{params.chatId}</div>
+  return <div className="
+  flex-1
+  justify-between
+  flex
+  flex-col
+  h-full
+  pl-6
+  pt-6
+  max-h-[calc(100vh-6rem)]
+  ">
+    <div className="
+    flex
+    sm:items-center
+    justify-between
+    py-3
+    border-b-2
+    border-gray-200">
+      <div className="
+      relative
+      flex
+      items-center
+      space-x-4">
+        <div className="relative">
+          <div className="
+          relative
+          w-8
+          sm:w-12
+          h-8
+          sm:h-12
+          ">
+            <Image
+              fill
+              referrerPolicy="no-referrer"
+              src={chatPartner.image}
+              alt={`${chatPartner.name} profile picture`}
+              className="rounded-full"
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col leading-tight">
+          <div className="text-xl flex items-center">
+            <span className="text-gray-700 mr-3 font-semibold">{chatPartner.name}</span>
+          </div>
+          <span className="text-sm text-gray-600">{chatPartner.email}</span>
+        </div>
+      </div>
+    </div>
+  </div>
 };
 
 export default page;
