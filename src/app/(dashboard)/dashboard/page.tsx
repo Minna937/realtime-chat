434c629fb648c3c0eb5f89498d20753058e84cd7
Minna <liu_minna@hotmail.com>
2023-06-16 18:52:56 +0200

    refreshes the added friend's page once accepted

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -3,9 +3,9 @@ import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 import { FC } from 'react';
 
-
 const page = async ({ }) => {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
+  
   
   return <pre>Dashboard</pre>
     ;
