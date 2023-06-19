cfe31ad7479c7c91d5dcd9ae9f92e391ce9164d8
Minna <liu_minna@hotmail.com>
2023-06-19 17:25:23 +0200

    friend request not showing up

--- src/components/FriendRequestsSidebarOption.tsx
+++ src/components/FriendRequestsSidebarOption.tsx
@@ -21,28 +21,31 @@ const FriendRequestsSidebarOption: FC<FriendRequestsSidebarOptionProps> = ({
 
     useEffect(() => {
         pusherClient.subscribe(
-            toPusherKey(`user:${sessionId}:incoming_friend_requests`)
-        );
-        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`));
-
+          toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+        )
+        pusherClient.subscribe(toPusherKey(`user:${sessionId}:friends`))
+    
         const friendRequestHandler = () => {
-            setUnseenRequestCount((prev) => prev + 1)
-        };
-
+          setUnseenRequestCount((prev) => prev + 1)
+        }
+    
         const addedFriendHandler = () => {
-            setUnseenRequestCount((prev) => prev - 1);
+          setUnseenRequestCount((prev) => prev - 1)
         }
-
-        pusherClient.bind('incoming_friend_requests', friendRequestHandler);
-        pusherClient.bind(`new_friend`, addedFriendHandler);
-
+    
+        pusherClient.bind('incoming_friend_requests', friendRequestHandler)
+        pusherClient.bind('new_friend', addedFriendHandler)
+    
         return () => {
-            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:incoming_friend_requests`));
-            pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`));
-            pusherClient.unbind('incoming_friend_requests', friendRequestHandler);
-            pusherClient.unbind(`new_friend`, addedFriendHandler);
+          pusherClient.unsubscribe(
+            toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+          )
+          pusherClient.unsubscribe(toPusherKey(`user:${sessionId}:friends`))
+    
+          pusherClient.unbind('new_friend', addedFriendHandler)
+          pusherClient.unbind('incoming_friend_requests', friendRequestHandler)
         }
-    }, [sessionId]);
+      }, [sessionId])
 
     return <Link href="/dashboard/requests"
         className="
