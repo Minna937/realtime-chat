259b8597d01984d9c24893ad543310d226fb64d4
Minna <liu_minna@hotmail.com>
2023-06-19 23:38:17 +0200

    adding await to pusher trigger

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -51,7 +51,7 @@ export async function POST(req: Request) {
 
         //valid request, send friend request
 
-        pusherServer.trigger(
+        await pusherServer.trigger(
             toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
             'incoming_friend_requests',
             {
